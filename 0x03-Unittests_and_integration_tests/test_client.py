f7130d984bb405c110a16ab4b7e351c11bd32211
Khalil Hafz <khalilhafz@gmail.com>
2025-11-15 17:30:03 +0200

    Unittests and Integration Tests task6 More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,7 +5,7 @@ Unit test suite for the `client` module.
 import unittest
 from parameterized import parameterized
 from client import GithubOrgClient
-from typing import Dict
+from typing import Dict, List
 from unittest.mock import patch, Mock, PropertyMock
 
 
@@ -66,3 +66,48 @@ class TestGithubOrgClient(unittest.TestCase):
 
             # Assert that the repos_url is the one from the payload
             self.assertEqual(repos_url, known_payload["repos_url"])
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json: Mock) -> None:
+        """
+        Tests that `GithubOrgClient.public_repos` returns the
+        correct list of repo names based on the mocked `repos_payload`.
+        """
+        # This is the payload that `get_json` will return
+        # (simulating the result of `repos_payload`)
+        repos_payload = [
+            {"name": "repo-one"},
+            {"name": "repo-two"},
+            {"name": "repo-three"}
+        ]
+        mock_get_json.return_value = repos_payload
+
+        # This is the URL that `_public_repos_url` will return
+        test_repos_url = "https://api.github.com/orgs/test/repos"
+
+        # Patch `_public_repos_url` as a property
+        with patch.object(GithubOrgClient,
+                          '_public_repos_url',
+                          new_callable=PropertyMock) as mock_public_repos_url:
+
+            # Set the return value for the mocked property
+            mock_public_repos_url.return_value = test_repos_url
+
+            # Instantiate the client
+            client = GithubOrgClient("test")
+
+            # Call the method under test
+            public_repos = client.public_repos()
+
+            # Define the expected list of repo names
+            expected_repos = ["repo-one", "repo-two", "repo-three"]
+
+            # Assert the list of repos is as expected
+            self.assertEqual(public_repos, expected_repos)
+
+            # Assert the mocked property was called once
+            mock_public_repos_url.assert_called_once()
+
+            # Assert `get_json` was called once with the correct URL
+            # (which came from the mocked property)
+            mock_get_json.assert_called_once_with(test_repos_url)
