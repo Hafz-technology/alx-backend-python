ad04bfa12b3cb72c59b901b7fab24c3c13baf12d
Khalil Hafz <khalilhafz@gmail.com>
2025-12-06 20:33:53 +0200

    Event Listeners (Signals), ORM & Advanced ORM Techniques task2

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,7 +1,10 @@
-from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.utils import timezone  # Import timezone
 from .models import Message, Notification, MessageHistory
+
+from django.db.models.signals import post_delete, post_save, pre_save
+from django.contrib.auth.models import User
+
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
@@ -32,6 +35,25 @@ def log_message_edit(sender, instance, **kwargs):
         except Message.DoesNotExist:
             pass
         
+
+
+@receiver(post_delete, sender=User)
+def cleanup_user_data(sender, instance, **kwargs):
+    """
+    Signal to clean up all related data when a user is deleted.
+    Note: If on_delete=models.CASCADE is set in models, this is redundant 
+    but ensures explicit data cleanup as per task requirements.
+    """
+    # Delete all messages sent by the user
+    Message.objects.filter(sender=instance).delete()
+    
+    # Delete all messages received by the user
+    Message.objects.filter(receiver=instance).delete()
+    
+    # Delete all notifications associated with the user
+    Notification.objects.filter(user=instance).delete()
+    
+
         
         
         
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+
+
+@login_required
+def delete_user(request):
+    if request.method == 'POST':
+        user = request.user
+        # The delete() method triggers the standard Django deletion process
+        # which includes cascading deletes defined in models.py
+        user.delete()
+        return redirect('/')  # Redirect to home or login page after deletion
+    
+    # If GET request, you might render a confirmation page
+    return render(request, 'messaging/delete_confirmation.html')
+
+
