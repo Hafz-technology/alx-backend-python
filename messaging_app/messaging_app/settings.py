5a01d54c6427846e6395efdbbe9f85d22c3cc536
Khalil Hafz <khalilhafz@gmail.com>
2025-12-07 23:37:43 +0200

    task1

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -157,3 +157,19 @@ SIMPLE_JWT = {
 
 AUTH_USER_MODEL = 'chats.User'
 
+
+import os
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': os.getenv('MYSQL_HOST', 'db'), # Defaults to 'db' service name
+        'PORT': os.getenv('MYSQL_PORT', '3306'),
+    }
+}
+
